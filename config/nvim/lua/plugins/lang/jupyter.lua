3dabcc23d09d63348678ccf3260fc51a5c7b60e0
blob1234567890 <ben.k.olson@gmail.com>
2026-04-18 20:13:03 +0900

    cleared out some unneeded keymaps and stuff

--- config/nvim/lua/plugins/lang/jupyter.lua
+++ config/nvim/lua/plugins/lang/jupyter.lua
@@ -16,7 +16,6 @@ return {
         config = {
           -- Whether a repl should be discarded or not
           scratch_repl = true,
-          -- Your repl definitions come here
           repl_definition = {
             sh = {
               -- Can be a table or a function that
@@ -28,28 +27,10 @@ return {
           -- See below for more information
           repl_open_cmd = require("iron.view").bottom(40),
         },
-        -- Iron doesn't set keymaps by default anymore.
-        -- You can set them here or manually add keymaps to the functions in iron.core
         keymaps = {
-          send_motion = "<space>sc",
-          visual_send = "<space>sc",
-          send_file = "<space>sf",
-          send_line = "<space>sl",
-          send_mark = "<space>sm",
-          mark_motion = "<space>mc",
-          mark_visual = "<space>mc",
-          remove_mark = "<space>md",
-          cr = "<space>s<cr>",
-          interrupt = "<space>s<space>",
-          exit = "<space>sq",
-          clear = "<space>cl",
+          send_motion = "ctr",
+          visual_send = "ctr",
         },
-        -- If the highlight is on, you can change how it looks
-        -- For the available options, check nvim_set_hl
-        highlight = {
-          italic = true,
-        },
-        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
       })
 
       -- iron also has a list of commands, see :h iron-commands for all available commands
