610f6d7db589162b96c3d174ce8bbca01c4324b9
blob1234567890 <ben.k.olson@gmail.com>
2026-04-18 14:41:37 +0900

    rename(lang.lua -> extras.lua)

--- config/nvim/lua/plugins/lang.lua
+++
@@ -1,9 +0,0 @@
-return {
-    { import = "lang/lang.lua.lua" },
-    { import = "lang/lang.markdown.lua" },
-    { import = "lang/lang.nix.lua" },
-    { import = "lang/lang.python.lua" },
-    { import = "lang/lang.rust.lua" },
-    { import = "lang/lang.tex.lua" },
-    { import = "lang/lang.wgsl.lua" },
-}
